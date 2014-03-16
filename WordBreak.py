800e1aeaee1b4899dc516052c49de258fc64c0d9
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-03-15 20:52:27 -0400

    Create WordBreak.py

---
+++ WordBreak.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    def wordPart(self,s,i,j,dict,cache=None):
+        if cache is None:
+            cache={}
+        if (i,j) in cache:
+            return cache[(i,j)]
+        if s[i:j] in dict:
+            cache[(i,j)]=True
+            return True
+        for k in xrange(i+1,j):
+            if self.wordPart(s,i,k,dict,cache) and self.wordPart(s,k,j,dict,cache):
+                cache[(i,j)]=True
+                return True
+        cache[(i,j)]=False
+        return False
+    def wordBreak(self, s, dict):
+        return self.wordPart(s,0,len(s),dict)
