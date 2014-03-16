8b26fe284b32637ab9552cc9ed28597d952c4f3e
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-03-15 22:04:17 -0400

    Update NQueensII.py

--- NQueensII.py
+++ NQueensII.py
@@ -3,23 +3,20 @@ class Solution:
     # @return an integer
     def no_attack(self,config,i):
         for j in xrange(i):
-            if abs(j-i)==abs(config[j]-config[i]):
+            if abs(j-i)==abs(config[j]-config[i]) or\
+            config[i]==config[j]:
                 return False
         return True
-    def dfs(self,i,n,config,V):
+    def dfs(self,i,n,config):
         if i==n:
             return 1
         total=0
         for j in xrange(n):
-            if j not in V:
-                config[i]=j
-                if self.no_attack(config,i):
-                    V.add(j)
-                    total+=self.dfs(i+1,n,config,V)
-                    V.remove(j)
+            config[i]=j
+            if self.no_attack(config,i):
+                total+=self.dfs(i+1,n,config)
         return total
         
     def totalNQueens(self, n):
         config=[-1]*n
-        V=set()
-        return self.dfs(0,n,config,V)
+        return self.dfs(0,n,config)
