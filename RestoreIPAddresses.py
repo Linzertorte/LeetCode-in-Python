9bd777666e2a3af4ed199c5ac3166d58dd34bb92
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-04-04 01:15:04 -0400

    Update RestoreIPAddresses.py

--- RestoreIPAddresses.py
+++ RestoreIPAddresses.py
@@ -1,17 +1,17 @@
 class Solution:
-    def dfs(self,s,sub,ips,ip):
-        if sub==4:
-            if s=="":
-                ips.append(ip[1:])
-            return
-        for i in range(1,4):
-            if i > len(s): continue
-            if int(s[:i])<=255:
-                self.dfs(s[i:],sub+1,ips,ip+'.'+s[:i])
-            if s[0]=='0': break
     # @param s, a string
     # @return a list of strings
     def restoreIpAddresses(self, s):
+        def dfs(s,sub,ips,ip):
+            if sub==4:
+                if s=="":
+                    ips.append(ip[1:])
+                return
+            for i in range(1,4):
+                if i > len(s): continue
+                if int(s[:i])<=255:
+                    dfs(s[i:],sub+1,ips,ip+'.'+s[:i])
+                if s[0]=='0': break
         ips=[]
-        self.dfs(s,0,ips,"")
+        dfs(s,0,ips,"")
         return ips
