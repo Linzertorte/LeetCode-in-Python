997d8af8dd2c4099e199a63dd5f8736558154208
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-07-10 07:40:46 -0400

    Update WildcardMatching.py

--- WildcardMatching.py
+++ WildcardMatching.py
@@ -1,60 +1,32 @@
-
 class Solution:
-    # @param s, an input string
-    # @param p, a pattern string
-    # @return a boolean
     def isMatch(self, s, p):
-
-        def match_from(i,s,p):
-            n = len(p)
-            if i+n > len(s):
+        def match_from(i,n, s,p):
+            m = len(p)
+            if i+m > n:
                 return False
-            for j in xrange(n):
+            for j in xrange(m):
                 if not (p[j]=='?' or p[j]==s[i+j]):
                     return False
             return True
-
-        if '*' not in p:
-            if len(s)!=len(p):
-                return False
-            return match_from(0,s,p)
-
-        l,r=p.index('*'),p.rindex('*')
-
-        #print l+len(p)-r-1
-
-        if len(s)<l+len(p)-r-1:
-            return False
-
-        sleft,pleft = s[:l],p[:l]
-        sright,pright = s[r-len(p)+1:],p[r-len(p)+1:]
-        if r-len(p)+1==0:
-            sright,pright = "",""
-
-
-        if not match_from(0,sleft,pleft):
-            return False
-        if not match_from(0,sright,pright):
-            return False
-
-        if r-len(p)+1==0:
-            s,p=s[l:],p[l:]
-        else:
-            s = s[l:r-len(p)+1]
-            p = p[l:r-len(p)+1]
-
         pp = ""
         for a in p:
             if a=='*' and len(pp)>0 and pp[-1]=='*':
                 continue #ignore extra *
             pp += a
-        chunks = pp.split('*')[1:-1]
-        begin = 0
-        m = len(s)
-        for chunk in chunks:
-            while begin<m and (not match_from(begin,s,chunk)):
+        chunks = pp.split('*')
+        cnt,n = len(chunks),len(s)
+        if cnt == 1:
+            return len(s)==len(p) and match_from(0,n,s,p)
+        l,r = len(chunks[0]),len(chunks[-1])
+        if l + r > n or not match_from(0,l,s,chunks[0]) or not match_from(n-r,n,s,chunks[-1]):
+            return False
+        begin = l
+        n -= r
+        for i in xrange(1,cnt - 1):
+            chunk = chunks[i]
+            while begin<n and (not match_from(begin, n, s,chunk)):
                 begin += 1
-            if begin == m:
+            if begin == n:
                 return False
             begin += len(chunk)
         return True
