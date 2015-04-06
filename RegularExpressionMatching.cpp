756ac7c256fa636d05272d60a26bbd943d6bb686
Linzertorte <Linzertorte@users.noreply.github.com>
2015-04-06 10:49:41 -0700

    Update RegularExpressionMatching.cpp

--- RegularExpressionMatching.cpp
+++ RegularExpressionMatching.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        return matchhere(p,s);
-    }
-    bool matchhere(const char *p,const char *s){
+    bool isMatch(const char *s,const char *p){
         if(*p=='\0') return *s=='\0';
         if(p[1]=='*') return matchstar(p[0],p+2,s);
-        if(*s!='\0' && (*p=='.' || *p==*s)) return matchhere(p+1,s+1);
+        if(*s!='\0' && (*p=='.' || *p==*s)) return isMatch(s+1,p+1);
         return false;
     }
     bool matchstar(int c, const char *p, const char *s){
         do {
             
-            if(matchhere(p,s)) return true;
+            if(isMatch(s,p)) return true;
         }while(*s!='\0' && (*s++ ==c || c=='.'));
         return false;
     }
