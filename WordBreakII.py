c49fddbceaea428accf3274a85b148f2c560f088
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 15:51:36 -0700

    Update WordBreakII.py

--- WordBreakII.py
+++ WordBreakII.py
@@ -1,29 +1,10 @@
-class Solution:
-    # @param s, a string
-    # @param dict, a set of string
-    # @return a list of strings
-    def wordBreak(self, s, dic):
-        def dfs(i,s,dic,sentence,sentences):
-            if i == len(s):
-                sentences.append(sentence[1:])
-                return
-            for j in xrange(i+1,len(s)+1):
-                if s[i:j] in dic:
-                    #print s[i:j]
-                    dfs(j,s,dic,sentence+" "+s[i:j],sentences)
-
-        sentences = []
-        s_cnt = [0]*128
-        w_cnt = [0]*128
-        for i in s:
-            s_cnt[ord(i)] = 1
-        for w in dic:
-            for i in w:
-                w_cnt[ord(i)] =1
-        for i in xrange(128):
-            if s_cnt[i]==1 and w_cnt[i]==0:
-                return sentences
-        if len(s)==0:
-            return sentences
-        dfs(0,s,dic,"",sentences)
-        return sentences
+class Solution(object):
+    def wordBreak(self, s, words):
+        dp, n = {}, len(s)
+        def dfs(i):
+            if i==n:
+                return ['']
+            if i not in dp:
+                dp[i] = [ ' '+ s[i:j+1] + w for j in xrange(i,n) if s[i:j+1] in words for w in dfs(j+1)]
+            return dp[i]
+        return map(lambda x:x[1:],dfs(0))
