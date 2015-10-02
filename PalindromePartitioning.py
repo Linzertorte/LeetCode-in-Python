5dac7ec35bde1ed04840425a4a828f977864a1b5
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 15:37:18 -0700

    Update PalindromePartitioning.py

--- PalindromePartitioning.py
+++ PalindromePartitioning.py
@@ -1,31 +1,15 @@
-class Solution:
-    # @param s, a string
-    # @return a list of lists of string
+class Solution(object):
     def partition(self, s):
-
-        def dfs(i,n,s,yes,path,result):
-            if i==n:
-                result.append(path[:])
-                return
-            for j in xrange(i+1,n+1):
-                if yes[i][j]:
-                    path.append(s[i:j])
-                    dfs(j,n,s,yes,path,result)
-                    path.pop()
-
-
-        n = len(s)
-        yes = [[0]*(n+1) for i in xrange(n+1)]
-        for i in xrange(n):
-            yes[i][i] = yes[i][i+1] = 1
-        for k in xrange(2,n+1):
-            for i in xrange(n):
-                j = i+k
-                if j>n:
-                    continue
-                if s[i]==s[j-1] and yes[i+1][j-1]:
-                    yes[i][j] = 1
-        result = []
-        path = []
-        dfs(0,n,s,yes,path,result)
-        return result
+        dp,result,acc = {},[],[]
+        def ok(i,j):
+            if i>j: return True
+            if (i,j) not in dp:
+                dp[i,j] = (s[i]==s[j] and ok(i+1,j-1))
+            return dp[i,j]
+        result = {}
+        def dfs(i):
+            if i==len(s): return [[]]
+            if i not in result:
+                result[i] = [ [s[i:j+1]] + p for j in xrange(i,len(s)) if ok(i,j) for p in dfs(j+1)]
+            return result[i]
+        return dfs(0)
