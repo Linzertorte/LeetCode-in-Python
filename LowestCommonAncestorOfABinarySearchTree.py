4fff0e6b558d0af6d844fccdeef280988d7164fc
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 16:43:45 -0700

    Update LowestCommonAncestorOfABinarySearchTree.py

--- LowestCommonAncestorOfABinarySearchTree.py
+++ LowestCommonAncestorOfABinarySearchTree.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        def answerAndCount(root,p,q):
-            if root is None: return (None,0)
-            la,lc = answerAndCount(root.left,p,q)
-            ra,rc = answerAndCount(root.right,p,q)
-            if la: return (la,lc)
-            if ra: return (ra,rc)
-            if root==p and root==q: return (root,2)
-            if root==p or root==q:
-                if lc==1 or rc==1: return (root,2)
-                else: return (None,1)
-            if lc==1 and rc==1: return (root,2)
-            return (None, lc+rc)
-        return answerAndCount(root,p,q)[0]
+        if root is None: return None
+        if root==p or root==q:
+            return root
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
+        if l and r: return root
+        if l: return l
+        if r: return r
+        return None
