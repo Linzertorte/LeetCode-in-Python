493e52168f1ef93e3e4d3e8dadd514a1c7811884
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 14:59:32 -0700

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,14 +1,10 @@
-
 class Solution(object):
     def reverseList(self, head):
         if not head: return head
         p,q,r = None,None,head
-        for i in xrange(2):
+        while r:
             if q: q.next = p
             p,q,r = q,r,r.next
             if not r: break
-        while r:
-            q.next = p
-            p,q,r = q,r,r.next
         q.next = p
         return q
