609c81e339ef1b159fcb118419433b7fdf09eacb
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 15:14:31 -0700

    Update WordLadder.py

--- WordLadder.py
+++ WordLadder.py
@@ -1,23 +1,21 @@
-class Solution:
-    lowercase = map(chr, range(97, 123))
-    def ladderLength(self, start, end, dic):
-        def nextWord(w,dic):
-            for i in xrange(len(w)):
-                for c in self.lowercase:
-                    w1 = w[:i]+c+w[i+1:]
-                    if c!=w[i] and w1 in dic:
-                        yield w1
-        dic.add(end)
-        dist = dict()
+class Solution(object):
+    def ladderLength(self, start, end, words):
+        lowercase = map(chr, range(97, 123))
+        words.add(end)
+        words.add(start)
+        def next_word(w):
+            return [w[:i]+c+w[i+1:] for i in xrange(len(w)) for c in lowercase if c!=w[i] and w[:i]+c+w[i+1:] in words]
+        queue, dist = collections.deque(), {}
         dist[start] = 1
-        queue = collections.deque()
+        words.remove(start)
         queue.append(start)
-        while len(queue)!=0:
+        while queue:
             head = queue.popleft()
-            if head == end:
+            if head==end:
                 return dist[head]
-            for w in nextWord(head,dic):
+            for w in next_word(head):
                 if w not in dist:
                     dist[w] = dist[head]+1
+                    words.remove(w)
                     queue.append(w)
         return 0
