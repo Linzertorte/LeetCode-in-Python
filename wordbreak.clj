98ecc858bcfa6f08e16f6caf352db9cebdd79793
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-03 09:55:21 -0700

    Update wordbreak.clj

--- wordbreak.clj
+++ wordbreak.clj
@@ -3,20 +3,18 @@
 (def s "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab")
 (def dict #{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"})
 
-
 (defn add-word [w s]
   (str " " w s))
-(defn dfs [i n s dict]
-  (if (= i (+ n 1)) '()
-      (concat
-       (if (contains? dict (subs s 0 i))
-         (map (fn [x] (add-word (subs s 0 i) x)) (word (subs s i) dict))
-         '())
-       (dfs (+ i 1) n s dict))))
 
 (defn word-break-helper [s dict]
-  (if (= s "") (list "") (dfs 1 (count s) s dict)))
-(def word (memoize word-break-helper))
+  (if (= s "")
+    (list "")
+    (apply concat (for [x (range 1 (+ (count s) 1))
+                        :when (contains? dict (subs s 0 x))]
+                    (map (fn [p] (add-word (subs s 0 x) p))
+                         (word (subs s x)
+                               dict))))))
 
+(def word (memoize word-break-helper))
 (defn word-break [s dict]
   (map (fn [x] (subs x 1)) (word s dict)))
