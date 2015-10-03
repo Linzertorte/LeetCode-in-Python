6b0209e7b467379618541c5ab21f5da6f1016a78
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-02 23:15:28 -0700

    Update wordbreak.clj

--- wordbreak.clj
+++ wordbreak.clj
@@ -1,5 +1,8 @@
 (def s "catsanddog")
 (def dict #{"cat", "cats", "and", "sand", "dog"})
+(def s "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab")
+(def dict #{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"})
+
 
 (defn add-word [w s]
   (str " " w s))
@@ -7,12 +10,13 @@
   (if (= i (+ n 1)) '()
       (concat
        (if (contains? dict (subs s 0 i))
-         (map (fn [x] (add-word (subs s 0 i) x)) (word-break-helper (subs s i) dict))
+         (map (fn [x] (add-word (subs s 0 i) x)) (word (subs s i) dict))
          '())
        (dfs (+ i 1) n s dict))))
 
 (defn word-break-helper [s dict]
   (if (= s "") (list "") (dfs 1 (count s) s dict)))
+(def word (memoize word-break-helper))
 
 (defn word-break [s dict]
-  (map (fn [x] (subs x 1)) (word-break-helper s dict)))
+  (map (fn [x] (subs x 1)) (word s dict)))
