689e7a87b527d54e218a1a0a52d3cbf2348450f6
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-03 16:47:14 -0700

    Update SudokuSolver.py

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,37 +1,38 @@
-class Solution:
-
+class Solution(object):
     def solveSudoku(self, board):
-
-        def no_conflict(r,c,board):
-            at = board[r][c]
-            for k in xrange(9):
-                if k!=c and board[r][k]==at:
-                    return False
-            for k in xrange(9):
-                if k!=r and board[k][c]==at:
-                    return False
-            box_r = (r/3)*3
-            box_c = (c/3)*3
-            for i in xrange(box_r,box_r+3):
-                for j in xrange(box_c,box_c+3):
-                    if (i!=r or j!=c) and board[i][j]==at:
-                        return False
-            return True
-
-        def dfs(r,c,board):
-            if r==9:
-                return True
-            if c==9:
-                return dfs(r+1,0,board)
-            if board[r][c]!='.':
-                if dfs(r,c+1,board):
-                    return True
-            else:
-                for k in xrange(1,10):
-                    board[r][c]=str(k)
-                    if no_conflict(r,c,board) and dfs(r,c+1,board):
+        print board
+        col,row,box=[set() for i in xrange(9)],[set() for i in xrange(9)],[set() for i in xrange(9)]
+        def box_id(i,j):
+            return (i/3)*3 + j/3
+        def can_place(i,j,x):
+            return (x not in row[i]) and (x not in col[j]) and (x not in box[box_id(i,j)])
+        def place(i,j,x):
+            row[i].add(x)
+            col[j].add(x)
+            box[box_id(i,j)].add(x)
+        def remove(i,j,x):
+            row[i].remove(x)
+            col[j].remove(x)
+            box[box_id(i,j)].remove(x)
+        
+        for i in xrange(0,9):
+            for j in xrange(0,9):
+                if board[i][j]!='.':
+                    place(i,j,board[i][j])
+        print col, row, box
+        def dfs(i,j):
+            print '(',i,j,')'
+            if i==9: return True
+            if j==9: return dfs(i+1,0)
+            if board[i][j]!='.': return dfs(i,j+1)
+            for x in xrange(1,10):
+                x = str(x)
+                if can_place(i,j,x):
+                    board[i][j]=x
+                    place(i,j,x)
+                    if dfs(i,j+1):
                         return True
-                    board[r][c]='.'
+                    board[i][j]='.'
+                    remove(i,j,x)
             return False
-
-        dfs(0,0,board)
+        dfs(0,0)
