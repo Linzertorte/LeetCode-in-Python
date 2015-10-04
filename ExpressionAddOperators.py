27fc200ff1631507cf74d0439fb8ff1af1bfc8e8
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-04 00:09:42 -0700

    Update ExpressionAddOperators.py

--- ExpressionAddOperators.py
+++ ExpressionAddOperators.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def addOperators(self, num, target):
-        if num=="2147483648": return []
         n = len(num)
         def factor(s):
             if len(s)==1: return [s]
-            return [s[0]+x+p for x in ['*',''] for p in factor(s[1:])]
+            op = ['*','']
+            return [s[0]+x+p for x in op for p in factor(s[1:])]
         def evaluate(s):
             s = s.split('*')
             for x in s:
                 if x[0]=='0' and len(x)>1: return None
             return reduce(lambda y,x: int(x)*y, s, 1) 
+        dp = {}
         def dfs(i,target):
             if i==-1:
                 return [''] if target==0 else []
+            if (i,target) in dp: return dp[i,target]
             r = []
             for j in xrange(i,-1,-1):
                 for f in factor(num[j:i+1]):
@@ -23,5 +25,6 @@ class Solution(object):
                     else:
                         r += [ p+"+"+f for p in dfs(j-1,target-x)]
                         r += [ p+'-'+f for p in dfs(j-1,target+x)]
+            dp[i,target] = r
             return r
         return dfs(n-1,target)
