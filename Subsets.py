998f096f0dd474a91e86fce8adef41623e490e6a
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-04 00:42:44 -0700

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -1,13 +1,8 @@
-class Solution:
-    def dfs(self,cur,n,S,sets,one):
-        if cur==n:
-            sets.append(one)
-            return
-        self.dfs(cur+1,n,S,sets,one+[])
-        self.dfs(cur+1,n,S,sets,one+[S[cur]])
-    # @param S, a list of integer
-    # @return a list of lists of integer
-    def subsets(self, S):
-        sets=[]
-        self.dfs(0,len(S),sorted(S),sets,[])
-        return sets
+class Solution(object):
+    def subsets(self, nums):
+        nums = sorted(nums)
+        n = len(nums)
+        def dfs(i):
+            if i==n: return [[]]
+            return [x+p for x in [[nums[i]],[]] for p in dfs(i+1)]
+        return dfs(0)
