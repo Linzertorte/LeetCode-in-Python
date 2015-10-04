6ce8a1f6382863d66ce84ead8e4e39cdfaf90331
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-04 12:51:10 -0700

    Update LargestNumber.py

--- LargestNumber.py
+++ LargestNumber.py
@@ -3,28 +3,10 @@ class Solution(object):
         nums = map(str,nums)
         def cmp(x,y):
             return x+y>y+x
-        def mergeSort(nums):
-            n = len(nums)
-            if n<=1: return nums
-            mid = n/2
-            left = mergeSort(nums[:mid])
-            right = mergeSort(nums[mid:])
-            i,j,n,m = 0,0,len(left),len(right)
-            x = []
-            while i<n and j<m:
-                if cmp(left[i],right[j]):
-                    x.append(left[i])
-                    i+=1
-                else:
-                    x.append(right[j])
-                    j+=1
-            while i<n:
-                x.append(left[i])
-                i+=1
-            while j<m:
-                x.append(right[j])
-                j+=1
-            return x
-        x =  ''.join(mergeSort(nums))
+        def quickSort(nums):
+            if len(nums)<=1: return nums
+            pivot = nums[0]
+            return quickSort(filter(lambda x:cmp(x,pivot), nums[1:]))+[pivot]+quickSort(filter(lambda x:not cmp(x,pivot),nums[1:]))
+        x=''.join(quickSort(nums))
         while len(x)>1 and x[0]=='0': x=x[1:]
         return x
