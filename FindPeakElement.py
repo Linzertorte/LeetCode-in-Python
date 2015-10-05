2cf670a8af1c2909563fd41a8b120eee33dc5099
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-05 11:33:08 -0700

    Update FindPeakElement.py

--- FindPeakElement.py
+++ FindPeakElement.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def findPeakElement(self, nums):
         n = len(nums)
-        l,r = 0,n-1
+        l,r = -1,n-1
         while l+1<r:
             mid = (l+r)/2
             if nums[mid]>nums[mid+1]: r=mid
             else: l=mid
-        return r if nums[r]>nums[0] else 0
+        return r
