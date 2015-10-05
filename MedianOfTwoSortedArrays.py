a826cf623f2b050b8a5818227d6ef413132308a2
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-04 22:23:32 -0700

    Update MedianOfTwoSortedArrays.py

--- MedianOfTwoSortedArrays.py
+++ MedianOfTwoSortedArrays.py
@@ -1,17 +1,15 @@
-#not good solution...
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         def kth(x,y,k):
             n,m = len(x),len(y)
             if n==0: return y[k-1]
             if m==0: return x[k-1]
-            if n<=2 or m<=2: return sorted(x+y)[k-1]
             xm,ym=n/2,m/2
             if x[xm]>y[ym]:
                 x,y,xm,ym,n,m = y,x,ym,xm,m,n
-            if k<=xm+ym:
+            if k<=xm+ym+1:
                 return kth(x,y[:ym],k)
-            else: return kth(x[xm:],y,k-xm)
+            else: return kth(x[xm+1:],y,k-xm-1)
         n,m = len(nums1),len(nums2)
         k = (n+m)/2
         if (n+m)%2==1:
