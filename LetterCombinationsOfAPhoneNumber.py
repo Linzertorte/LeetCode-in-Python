1061e7661a0c433bd8ff3848e9ea05ff39c5a964
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-05 16:38:08 -0700

    Update LetterCombinationsOfAPhoneNumber.py

--- LetterCombinationsOfAPhoneNumber.py
+++ LetterCombinationsOfAPhoneNumber.py
@@ -1,10 +1,7 @@
-class Solution:
-    # @return a list of strings, [s1, s2]
+class Solution(object):
     def letterCombinations(self, digits):
         letter={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxzy'}
-        if digits=='':
-            return [""]
-        comb=list(letter[digits[0]])
-        for i in xrange(1,len(digits)):
-            comb=[x+y for x in comb for y in letter[digits[i]]]
-        return comb
+        def dfs(digits):
+            if len(digits)==0: return ['']
+            return [x+p for p in dfs(digits[1:]) for x in letter[digits[0]]]
+        return dfs(digits) if digits else []
