f6379dd2ed8f208979997f42d87685d611439344
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-05 19:11:39 -0700

    Update BinaryTreePostorderTraversal.py

--- BinaryTreePostorderTraversal.py
+++ BinaryTreePostorderTraversal.py
@@ -1,30 +1,13 @@
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    # @param root, a tree node
-    # @return a list of integers
+class Solution(object):
     def postorderTraversal(self, root):
-        #a config a {'node':TreeNode,'flag':0-2}
-        stack=[]
-        l=[]
-        if root is None:
-            return l
-        stack.append({'node':root,'flag':0})
-        while len(stack)!=0:
-            if stack[-1]['flag']==0: #left child
-                stack[-1]['flag']+=1
-                if stack[-1]['node'].left is not None:
-                    stack.append({'node':stack[-1]['node'].left,'flag':0})
-            elif stack[-1]['flag']==1:
-                stack[-1]['flag']+=1
-                if stack[-1]['node'].right is not None:
-                    stack.append({'node':stack[-1]['node'].right,'flag':0})
+        st = [(root,0)]
+        p = []
+        if not root: return p
+        while st:
+            h,c = st.pop()
+            if c==1 or (not h.left and not h.right): p.append(h.val)
             else:
-                l.append(stack[-1]['node'].val)
-                stack.pop()
-        return l
+                st.append((h,1))
+                if h.right: st.append((h.right,0))
+                if h.left: st.append((h.left,0))
+        return p
