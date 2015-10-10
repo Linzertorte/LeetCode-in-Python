b802099269b836b0e0506ffae8424ac50e27da85
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-09 18:55:11 -0700

    Update BinaryTreeLevelOrderTraversal.py

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -1,26 +1,8 @@
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-class Solution:
-    def getNextLevel(self,level):
-        next=[]
-        for node in level:
-            if node.left:
-                next.append(node.left)
-            if node.right:
-                next.append(node.right)
-        return next
-    # @param root, a tree node
-    # @return a list of lists of integers
+class Solution(object):
     def levelOrder(self, root):
-        levels=[]
-        if not root:
-            return levels
-        level=[root]
-        while len(level)!=0:
-            levels.append(map((lambda x:x.val),level))
-            level=self.getNextLevel(level)
-        return levels
+        def level(xs):
+            if len(xs)==0: return []
+            cur = map(lambda x:x.val, xs)
+            return [cur] + level([j for i in xs for j in [i.left,i.right] if j])
+        if root is None: return []
+        return level([root])
